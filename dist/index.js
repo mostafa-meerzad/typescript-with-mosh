d586dd62edacc0e5183369e46d517b6e700e8daf
mostafa-meerzad <71199539+mostafa-meerzad@users.noreply.github.com>
2023-08-19 16:20:50 +0430

    complete adding and using a debugger-session in vscode

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,7 @@
 "use strict";
 let age = 23;
-if (age < 10) {
+if (age > 10) {
     age += 10;
 }
+console.log(age);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-let age: number = 23
+let age: number = 23;
 
-if (age < 10) {
-    age += 10
+if (age > 10) {
+  age += 10;
 }
+console.log(age);
